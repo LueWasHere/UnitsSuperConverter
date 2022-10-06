a74d52c08bb3426d6f467d8271541c90cdb08964
LueWasHere <82827429+LueWasHere@users.noreply.github.com>
2022-10-05 22:23:00 -0600

    Conversion Additions

--- bot.py
+++ bot.py
@@ -5,6 +5,11 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 import conversion_parser as cplib
+
+mk = 0
+unit = ""
+unit_to = ""
+hctd = 0
 try:
     with open("env.txt", "r") as ENVFile:
         token = ENVFile.read()
@@ -27,16 +32,18 @@ def start(update: Update, context: CallbackContext):
 
 def define_unit(update: Update, context: CallbackContext):
     pass
+    
 def help(update: Update, context: CallbackContext):
-    update.message.reply_text("/help - Display this message\n/start - Check if the bot is ready\n/define_unit OR /du - Define a new unit that USC will add to its database of units")
+    update.message.reply_text("/help - Display this message\n/start - Check if the bot is ready\n/define_unit OR /du [unit] [convert] [how_many_in] - Define a new unit that USC will add to its database of units where unit is the unit you are defining, convert is the unit you're converting to, and how_many_in is how many units are in the unit you're converting to.")
         
 def unknown(update: Update, context: CallbackContext):
+    global hctd
     if update.message.text[0] == '/':
         update.message.reply_text(
         "Sorry '%s' is not a command" % update.message.text)
     else:
         update.message.reply_text(
-        "Sorry I can't convert that")
+        cplib.parse(update.message.text, hctd))
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
@@ -44,5 +51,11 @@ updater.dispatcher.add_handler(CommandHandler('du', define_unit))
 updater.dispatcher.add_handler(CommandHandler('define_unit', define_unit))
 
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+
+with open("conversions.hctd", "r") as conversions:
+    hctd = conversions.read()
+    conversions.close()
+
+print(cplib.parse_hctd(hctd))
 
 updater.start_polling()
--- conversion_parser.py
+++ conversion_parser.py
@@ -1,4 +1,32 @@
-def parse():
-    pass
-def parse_hctd():
-    pass
+def parse(parsse, hctd_src):
+    parsse = parsse.split(' ')
+    unit = ""
+    to = ""
+    amount = ""
+    try:
+        amount = int(parsse[0])
+    except:
+        return "Fail, Enter a number. Conversion structure: [amount of units] [unit to convert] [unit to convert to]"
+    try:
+        unit = parsse[1]
+        to = parsse[2]
+    except:
+        return "Fail. Conversion structure: [amount of units] [unit to convert] [unit to convert to]"
+    
+    #for i in range()
+
+    abstract = ["sub", "add", "mul", "div"]
+    rep = ["-", "+", "*", "/"]
+    return f"{amount}{rep}"
+    
+def parse_hctd(parsse):
+    parsse = parsse.split('\n')
+    for i in range(0, len(parsse)):
+        parsse[i] = parsse[i].split(" ")
+    for i in range(0, len(parsse)):
+        try:
+            if parsse[i][0] == '#':
+                parsse.remove(parsse[i])
+        except:
+            print(f"Err in {parsse}")
+    return parsse
